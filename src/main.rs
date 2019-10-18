4639318e9af2218d6e2720598cd8d4a79ca2d711
William Speirs <bill.speirs@gmail.com>
2019-10-17 22:26:31 -0400

    First cut at putting everything together

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,138 @@
+extern crate clap;
 extern crate ring;
 extern crate zstd;
 
-use std::io::{self, Read, BufReader, BufWriter, Cursor};
-use std::io::prelude::*;
-use std::fs::File;
-
-
-use ring::aead::*;
-use ring::pbkdf2::*;
-use ring::rand::{SystemRandom, SecureRandom};
-use std::num::NonZeroU32;
-
-
-use zstd::block::Compressor;
-
 mod enc;
 
-//fn crypto() {
-//    let password = b"p@ssw0rd";
-//    let key_len = AES_256_GCM.key_len();
-//    let mut key = vec![0; key_len];
-//    let salt = [0, 1, 2, 3, 4, 5, 6, 7]; // would be account_id
-//    let iterations = unsafe { NonZeroU32::new_unchecked(100) };
-//
-//    derive(PBKDF2_HMAC_SHA512, iterations, &salt, &password[..], &mut key);
-//
-//    let unbound_key = UnboundKey::new(&AES_256_GCM, &key).expect("Error creating unbound key");
-//
-//    // Fill nonce with random data
-//    let rand = SystemRandom::new();
-//
-//    let less_safe_key = LessSafeKey::new(unbound_key);
-//
-//    let mut plaintext_1 = b"my secret message".to_vec();
-//    let mut plaintext_2 = b"my second secret message".to_vec();
-//
-//    println!("PLAINTEXT: {} {}", String::from_utf8(plaintext_1.clone()).unwrap(), String::from_utf8(plaintext_2.clone()).unwrap());
-//
-//    let mut cipher_text = Vec::new();
-//    let mut lens = Vec::new();
-//
-//    let mut nonce_buff1 = [0; 12];
-//    rand.fill(&mut nonce_buff1).expect("Error filling nonce");
-//    let nonce1 = Nonce::assume_unique_for_key(nonce_buff1);
-//
-//    less_safe_key.seal_in_place_append_tag(nonce1, Aad::empty(), &mut plaintext_1).expect("Could not seal");
-//    lens.push(plaintext_1.len());
-//    cipher_text.extend(plaintext_1);
-//
-//    let mut nonce_buff2 = [0; 12];
-//    rand.fill(&mut nonce_buff2).expect("Error filling nonce");
-//    let nonce2 = Nonce::assume_unique_for_key(nonce_buff2);
-//
-//    less_safe_key.seal_in_place_append_tag(nonce2, Aad::empty(), &mut plaintext_2).expect("Could not seal");
-//    lens.push(plaintext_2.len());
-//    cipher_text.extend(plaintext_2);
-//
-//    println!("LEN 1: {}", lens[0]);
-//    println!("LEN 2: {}", lens[1]);
-//    println!("CIPHERTEXT: {:?}", cipher_text);
-//
-//    let nonce1 = Nonce::assume_unique_for_key(nonce_buff1);
-//    let decrypted = less_safe_key.open_within(nonce1, Aad::empty(), &mut cipher_text[0..lens[0]], 0..).expect("Error opening");
-//
-//    println!("DECRYPT: {}", String::from_utf8(decrypted.to_vec()).unwrap());
-//
-//    let nonce2 = Nonce::assume_unique_for_key(nonce_buff2);
-//    let decrypted = less_safe_key.open_within(nonce2, Aad::empty(), &mut cipher_text[lens[0]..], 0..).expect("Error opening");
-//
-//    println!("DECRYPT: {}", String::from_utf8(decrypted.to_vec()).unwrap());
-//}
+use zstd::block::{Compressor, Decompressor};
+use clap::{Arg, App, SubCommand};
+use ring::rand::{SystemRandom, SecureRandom};
 
 use std::env;
+use std::io::*;
+use std::collections::HashMap;
+use std::fs;
+
+use enc::{Sealer, Opener};
+
+struct FileHeader {
+    version: u32,
+    salt: Vec<u8>,
+    nonce_seed: Vec<u8>,
+    comments: HashMap<String, String>
+}
+
 
 fn main() {
-    let mut stdin = BufReader::new(io::stdin());
-    let mut stdout = BufWriter::new(io::stdout());
-    let mut compress = Compressor::new();
+    let matches = App::new("Seal")
+        .version("1.0")
+        .author("William Speirs <bill.speirs@gmail.com>")
+        .about("Compresses and encrypts data")
+        .arg(Arg::with_name("input")
+            .short("i")
+            .long("input")
+            .value_name("FILE")
+            .help("Input (plaintext or ciphertext)")
+            .takes_value(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("FILE")
+            .help("Output (plaintext or ciphertext)")
+            .takes_value(true))
+        .arg(Arg::with_name("password")
+            .short("p")
+            .long("password")
+            .value_name("PASSWORD")
+            .required(true)
+            .help("The password used to encrypt or decrypt. Keep this secret!!!")
+            .takes_value(true))
+        .arg(Arg::with_name("salt")
+            .short("s")
+            .long("salt")
+            .value_name("SALT")
+            .help("The salt used to derive the key. No need to keep this secret.")
+            .takes_value(true))
+        .arg(Arg::with_name("encrypt")
+            .conflicts_with("decrypt")
+            .short("e")
+            .long("encrypt")
+            .help("Encrypts the input, and writes ciphertext to the output"))
+        .arg(Arg::with_name("decrypt")
+            .conflicts_with("encrypt")
+            .short("d")
+            .long("decrypt")
+            .help("Decrypts the input, and writes plaintext to the output"))
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+        .get_matches();
 
-    let args = env::args().collect::<Vec<_>>();
 
-    let compress_level = args[1].parse::<i32>().expect("Didn't pass compress level");
+    let input = matches.value_of("input").unwrap_or("-");
+    let output = matches.value_of("output").unwrap_or("-");
+    let password = matches.value_of("password").expect("Must supply a password!");
+    let encrypt = matches.is_present("encrypt");
 
-    loop {
-        let mut buff = vec![0; 262144];
-        let res = stdin.read(&mut buff);
+    // construct the input reader from either STDIN or from the file
+    let mut input_reader : Box<dyn BufRead> = if input == "-" {
+        Box::new(BufReader::new(stdin()))
+    } else {
+        Box::new(BufReader::new(fs::OpenOptions::new()
+            .read(true)
+            .write(false)
+            .create(false)
+            .open(input).expect("Error opening input file")))
+    };
 
-        if let Err(e) = res {
-            eprintln!("Error reading from STDIN: {:?}", e);
-            break;
+    // construct the output writer from either STDOUT or from the file
+    let mut output_writer : Box<dyn Write> = if output == "-" {
+        Box::new(BufWriter::new(stdout()))
+    } else {
+        Box::new(BufWriter::new(fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(output).expect("Error opening output file")))
+    };
+
+    let mut buff = vec![0; 4_048_576];
+    let mut rand = SystemRandom::new();
+
+    if encrypt {
+        let salt = matches.value_of("salt").expect("Must supply salt!");
+        let mut compress = Compressor::new();
+        let mut nonce_seed = [0; 8];
+
+        rand.fill(&mut nonce_seed);
+
+        let mut sealer = Sealer::new(password, salt.as_bytes(), nonce_seed);
+
+        loop {
+            let res = input_reader.read(&mut buff);
+
+            if let Err(e) = res {
+                eprintln!("Error reading input: {:?}", e);
+                break;
+            }
+
+            let amt = res.unwrap();
+
+            if amt == 0 {
+                break;
+            }
+
+            let plaintext = compress.compress(&buff[0..amt], 2).expect("Error compressing input");
+            let ciphertext = sealer.seal(plaintext);
+
+            output_writer.write_all(&ciphertext).expect("Error writing to output");
         }
+    } else {
+        let mut decompress = Decompressor::new();
+//        let mut opener = Opener::new(password, salt.as_bytes(), [1,2,3,4,5,6,7,8]);
 
-        let amt = res.unwrap();
-
-        if amt == 0 {
-            break;
-        }
-
-        let res = compress.compress(&buff[0..amt], compress_level);
-
-        if let Err(e) = res {
-            eprintln!("Error compressing block: {:?}", e);
-            break;
-        }
-
-        let buff = res.unwrap();
-
-        let res = stdout.write_all(buff.as_slice());
-
-        if let Err(e) = res {
-            eprintln!("Error writing to STDOUT: {:?}", e);
-            break;
-        }
     }
-
-
 
 }
